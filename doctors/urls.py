c34d3c529a8aaf6654c146dde73a05b4bc7db8dd
Norbey <norbey97gutierrez@gmail.com>
2025-10-08 18:00:23 -0500

    crud para doctor availability y medical note creado

--- doctors/urls.py
+++ doctors/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path
 
-from .views import list_doctors, detail_doctor, list_department, detal_department
+from .views import (
+    list_doctors, detail_doctor, 
+    list_department, detal_department, 
+    list_doctor_availability, detail_doctor_availability,
+    list_medical_note, detail_medical_note
+)
 
 
 urlpatterns = [
     path('doctors/', list_doctors),
     path('doctors/<int:pk>/', detail_doctor),
     path('department/', list_department),
-    path('department/<pk>/', detal_department)
+    path('department/<int:pk>/', detal_department),
+    path('doctor-availability/', list_doctor_availability),
+    path('doctor-availability/<int:pk>/', detail_doctor_availability),
+    path('note-medical/', list_medical_note),
+    path('note-medical/<int:pk>', detail_medical_note),
 ]
--- doctors/views.py
+++ doctors/views.py
@@ -1,5 +1,5 @@
-from .serializers import DoctorSerializer, DepartmentSerializer 
-from .models import Doctor, Department, DoctorAvailability
+from .serializers import DoctorSerializer, DepartmentSerializer, DoctorAvailabilitySerializer, MedicalNoteSerializer
+from .models import Doctor, Department, DoctorAvailability, MedicalNote
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -92,8 +92,87 @@ def detal_department(request, pk):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-# GET api/department/ - Listar todos los departamentos
-# POST api/department/ - Crear un nuevo departamento
-# GET api/department/<pk>/ - Detalle
-# PUT api/department/<pk>/ - Modifiacion
-# DELETE api/department/<pk>/ - Borrar
+# GET api/doctor-availability/ - Listar todos los medicos
+# POST api/doctor-availability/ - Crear una nueva disponibilidad de medicos
+# GET api/doctor-availability/<pk>/ - Detalle
+# PUT api/doctor-availability/<pk>/ - Modifiacion
+# DELETE api/doctor-availability/<pk>/ - Borrar
+
+
+@api_view(['GET', 'POST'])
+def list_doctor_availability(request):
+    if request.method == 'GET':
+        doctor_availabilitys = DoctorAvailability.objects.all()
+        serializer = DoctorAvailabilitySerializer(doctor_availabilitys, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = DoctorAvailabilitySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def detail_doctor_availability(request, pk):
+    try:
+        doctor_availability = DoctorAvailability.objects.get(id=pk)
+    except DoctorAvailability.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = DoctorAvailabilitySerializer(doctor_availability)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        serializer = DoctorAvailabilitySerializer(doctor_availability, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    if request.method == 'DELETE':
+        doctor_availability.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# GET api/note-medical/ - Listar todas las notas medicas
+# POST api/note-medical/ - Crear una nueva nota medica
+# GET api/note-medical/<pk>/ - Detalle
+# PUT api/note-medical/<pk>/ - Modifiacion
+# DELETE api/note-medical/<pk>/ - Borrar
+
+
+@api_view(['GET', 'POST'])
+def list_medical_note(request):
+    if request.method == 'GET':
+        medical_notes = MedicalNote.objects.all()
+        serializer = MedicalNoteSerializer(medical_notes, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = MedicalNoteSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def detail_medical_note(request, pk):
+    try:
+        medical_note = MedicalNote.objects.get(id=pk)
+    except MedicalNote.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = MedicalNoteSerializer(medical_note)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        serializer = MedicalNoteSerializer(medical_note, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    if request.method == 'DELETE':
+        medical_note.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
